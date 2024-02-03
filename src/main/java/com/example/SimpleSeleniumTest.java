7f965ca3706caae313f4650ff62e208d33645ba4
BobikGavvv <andrey678a@gmail.com>
2024-02-03 21:20:37 +0200

    .

---
+++ src/main/java/com/example/SimpleSeleniumTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleSeleniumTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        WebElement element = driver.findElement(By.className("o3j99"));
+        System.out.println(element.getSize());
+    }
+}
