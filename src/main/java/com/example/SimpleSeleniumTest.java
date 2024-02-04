ac82c28e95425426eccfe203729335dbc33b9805
BobikGavvv <andrey678a@gmail.com>
2024-02-04 21:05:39 +0200

    ,

--- src/main/java/com/example/SimpleSeleniumTest.java
+++ src/main/java/com/example/SimpleSeleniumTest.java
@@ -1,6 +1,6 @@
 package com.example;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,7 +10,25 @@ public class SimpleSeleniumTest {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
-        WebElement element = driver.findElement(By.className("o3j99"));
-        System.out.println(element.getSize());
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("интересные факты про котов вики");
+        element.sendKeys(Keys.RETURN);
+
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement firstLink = driver.findElement(By.cssSelector(".LC20lb.MBeuO.DKV0Md"));
+        System.out.println("Текст первой ссылки: " + firstLink.getText());
+        firstLink.click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
+
